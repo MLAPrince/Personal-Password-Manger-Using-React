94ebfad0e21e0cb3a12736cf1d79e4318e7cc396
MLAPrince <110454392+MLAPrince@users.noreply.github.com>
2025-07-18 14:33:53 +0500

    Update Passwords.jsx

--- src/components/Passwords.jsx
+++ src/components/Passwords.jsx
@@ -38,7 +38,6 @@ const Passwords = () => {
 
   return (
     <div className='text-white'>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] "></div>
       <h1 className='text-4xl text-center mt-5 text-lime-500'>
         Saved Passwords
       </h1>
